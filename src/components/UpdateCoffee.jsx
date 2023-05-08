30296f66dbc99a3c60f33c701a572f2fb230d077
Rownokzahan <84782117+Rownokzahan@users.noreply.github.com>
2023-05-09 02:10:12 +0600

    Add Update Functionality

--- src/components/UpdateCoffee.jsx
+++ src/components/UpdateCoffee.jsx
@@ -1,19 +1,52 @@
+import { useLoaderData } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const UpdateCoffee = () => {
+
+    const { _id, name, supplier, category, chef, taste, details, photo } = useLoaderData();
 
     const handleAddCoffee = event => {
         event.preventDefault();
         const form = event.target;
-        const name = form.name.value;
-        const supplier = form.supplier.value;
-        const category = form.category.value;
-        const chef = form.chef.value;
-        const taste = form.taste.value;
-        const details = form.details.value;
-        const photo = form.photo.value;
+        const updated_name = form.updated_name.value;
+        const updated_supplier = form.updated_supplier.value;
+        const updated_category = form.updated_category.value;
+        const updated_chef = form.updated_chef.value;
+        const updated_taste = form.updated_taste.value;
+        const updated_details = form.updated_details.value;
+        const updated_photo = form.updated_photo.value;
 
-        console.log(`Name: ${name}, Supplier: ${supplier}, Category: ${category}, Chef: ${chef}, Taste: ${taste}, Details: ${details}, Photo: ${photo}`);
+        const updateCoffee = {
+            updated_name,
+            updated_supplier,
+            updated_category,
+            updated_chef,
+            updated_taste,
+            updated_details,
+            updated_photo
+        };
 
+        fetch(`http://localhost:5000/coffee/${_id}`, {
+            method: "PUT",
+            body: JSON.stringify(updateCoffee),
+            headers: {
+                "content-type": "application/json"
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                form.reset();
+                if (data.modifiedCount) {
+                    Swal.fire({
+                        title: 'Success!',
+                        text: 'Coffee Updated Sucessfully',
+                        icon: 'success',
+                        confirmButtonText: 'Cool'
+                    })
+                }
+
+            })
+            .catch(error => console.log(error))
     }
 
     return (
@@ -25,38 +58,38 @@ const UpdateCoffee = () => {
                 <div className="grid md: grid-cols-2 gap-6">
                     <div>
                         <div className="flex flex-col gap-4 mb-6">
-                            <label className="font-semibold text-xl" htmlFor="name">Name</label>
-                            <input className="py-2 px-3 rounded" type="text" name="name" id="name" placeholder="Americano Coffee" />
+                            <label className="font-semibold text-xl" htmlFor="updated_name">Name</label>
+                            <input className="py-2 px-3 rounded" type="text" name="updated_name" id="updated_name" placeholder="Americano Coffee" defaultValue={name} />
                         </div>
                         <div className="flex flex-col gap-4 mb-6">
-                            <label className="font-semibold text-xl" htmlFor="supplier">Supplier</label>
-                            <input className="py-2 px-3 rounded" type="text" name="supplier" id="supplier" placeholder="Cappu Authorizer" />
+                            <label className="font-semibold text-xl" htmlFor="updated_supplier">Supplier</label>
+                            <input className="py-2 px-3 rounded" type="text" name="updated_supplier" id="updated_supplier" placeholder="Cappu Authorizer" defaultValue={supplier} />
                         </div>
 
                         <div className="flex flex-col gap-4 mb-6">
-                            <label className="font-semibold text-xl" htmlFor="category">Category</label>
-                            <input className="py-2 px-3 rounded" type="text" name="category" id="category" placeholder="Americano" />
+                            <label className="font-semibold text-xl" htmlFor="updated_category">Category</label>
+                            <input className="py-2 px-3 rounded" type="text" name="updated_category" id="updated_category" placeholder="Americano" defaultValue={category} />
                         </div>
                     </div>
                     <div>
                         <div className="flex flex-col gap-4 mb-6">
-                            <label className="font-semibold text-xl" htmlFor="chef">Chef</label>
-                            <input className="py-2 px-3 rounded" type="text" name="chef" id="chef" placeholder="Mr. Matin Paul" />
+                            <label className="font-semibold text-xl" htmlFor="updated_chef">Chef</label>
+                            <input className="py-2 px-3 rounded" type="text" name="updated_chef" id="updated_chef" placeholder="Mr. Matin Paul" defaultValue={chef} />
                         </div>
                         <div className="flex flex-col gap-4 mb-6">
-                            <label className="font-semibold text-xl" htmlFor="taste">Taste</label>
-                            <input className="py-2 px-3 rounded" type="text" name="taste" id="taste" placeholder="Sweet and hot" />
+                            <label className="font-semibold text-xl" htmlFor="updated_taste">Taste</label>
+                            <input className="py-2 px-3 rounded" type="text" name="updated_taste" id="updated_taste" placeholder="Sweet and hot" defaultValue={taste} />
                         </div>
                         <div className="flex flex-col gap-4 mb-6">
-                            <label className="font-semibold text-xl" htmlFor="details">Details</label>
-                            <input className="py-2 px-3 rounded" type="text" name="details" id="details" placeholder="Espresso with hot water" />
+                            <label className="font-semibold text-xl" htmlFor="updated_details">Details</label>
+                            <input className="py-2 px-3 rounded" type="text" name="updated_details" id="updated_details" placeholder="Espresso with hot water" defaultValue={details} />
                         </div>
                     </div>
                 </div>
 
                 <div className="flex flex-col gap-4 mb-6">
-                    <label className="font-semibold text-xl" htmlFor="photo">Photo</label>
-                    <input className="py-2 px-3 rounded" type="text" name="photo" id="photo" placeholder="https://i.ibb.co/PGqMPr9/11.png" />
+                    <label className="font-semibold text-xl" htmlFor="updated_photo">Photo</label>
+                    <input className="py-2 px-3 rounded" type="text" name="updated_photo" id="updated_photo" placeholder="https://i.ibb.co/PGqMPr9/11.png" defaultValue={photo} />
                 </div>
 
                 <button type="submit" className="w-full bg-light-golden hover:bg-light-gray border border-deep-midnight text-deep-midnight hover:text-white text-2xl text-center font-rancho drop-shadow-md rounded">Update Coffee Details</button>
